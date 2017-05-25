e2d446de268044cc396b4bee0e1514ebbbb3e981
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-05-25 10:51:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Andrzej on 2017-05-25.
+ */
+
+var inputEl = document.querySelector('input');
+var buttonEl = document.querySelector('button');
+var ulEl = document.querySelector('ul');
+
+buttonEl.addEventListener('click', addTodo);
+
+function addTodo(){
+    var userInput = inputEl.value;
+    if (userInput.trim() == ''){
+        return;
+    }
+    var todoLi = document.createElement('LI');
+    todoLi.textContent = userInput;
+    todoLi.addEventListener('click', removeTodo);
+    ulEl.appendChild(todoLi);
+}
+
+function removeTodo(event){
+    var clickedLi = event.target;
+    clickedLi.parentNode.removeChild(clickedLi);
+}
